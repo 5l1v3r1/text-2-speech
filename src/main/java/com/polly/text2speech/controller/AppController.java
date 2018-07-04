c4a8041e197a102813c65457d1b7d5962aa57832
pradeepganesh90 <agpradeep1990@gmail.com>
2018-07-04 18:09:13 +0530

    Added ajax call to the service

--- src/main/java/com/polly/text2speech/controller/AppController.java
+++ src/main/java/com/polly/text2speech/controller/AppController.java
@@ -1,8 +1,11 @@
 package com.polly.text2speech.controller;
 
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.polly.text2speech.model.AudioMetadata;
 
 /**
  * @author pradeep_ga
@@ -14,8 +17,10 @@ public class AppController {
     /**
      * @return
      */
-    @RequestMapping(value="/get", method = RequestMethod.GET)
-    public String welcome(){
+    @RequestMapping(value="converttexttospeech", method = RequestMethod.POST)
+    public String convertTextToSpeech(@RequestBody AudioMetadata audioMetadata){
+    	System.out.println(audioMetadata.getPlaintext());
+    	System.out.println(audioMetadata.getVoice());
         return "Welcome Pradeep";
     }
     
